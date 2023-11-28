c958ac2d1321ef2b60dcb411f2717f34db168578
anas <o@gamil.com>
2023-11-28 13:43:04 +0200

    permission management

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\VerificationCodeEmail;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class RegisterController extends Controller
+{
+    public function checkEmail(Request $request)
+    {
+        $verificationCode = Str::random(6);
+        Mail::to($request->email)->send(new VerificationCodeEmail($verificationCode));
+        return response()->json(['success']);
+    }
+
+
+    public function register(Request $request)
+    {
+        $input = $request->all();
+        $input['type_level'] = 1;
+        $user = User::create($input);
+        $success['remember_token'] = $user->createToken('anas')->plainTextToken;
+        $success['nameUser'] = $user->nameUser;
+        $success['id_user'] = $user->id_user;
+        $success['email'] = $user->email;
+        return response()->json([$success]);
+    }
+}
