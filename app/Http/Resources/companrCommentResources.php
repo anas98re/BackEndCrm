5ed6c24c79015e867dd0e51e92d2ce7a0cea3aae
anas <o@gamil.com>
2024-01-28 14:52:33 +0300

    (string)$this->id_comment_company

--- app/Http/Resources/companrCommentResources.php
+++ app/Http/Resources/companrCommentResources.php
@@ -9,9 +9,9 @@ class companrCommentResources extends JsonResource
     public function toArray($request)
     {
         return [
-            'id_comment_company' => $this->id_comment_company,
-            'fk_user' => (int)$this->fk_user,
-            'fk_company' => (int)$this->fk_company,
+            'id_comment_company' => (string)$this->id_comment_company,
+            'fk_user' => (string)$this->fk_user,
+            'fk_company' => (string)$this->fk_company,
             'content' => $this->content,
             'date_comment' => $this->date_comment,
             'nameUser' => $this->Users->nameUser,
