ba571d3ffb9fd09395e204f93ac7419c16e4d8d7
anas <o@gamil.com>
2024-01-30 14:49:45 +0300

    as

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\KnowCurrentUser::class,
         ],
     ];
 
@@ -73,5 +74,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'KnowCurrentUser' => \App\Http\Middleware\KnowCurrentUser::class,
     ];
 }
