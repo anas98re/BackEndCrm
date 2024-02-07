267f3c19954f3365e303fb407245b0366ed7cdff
anas <o@gamil.com>
2024-02-07 12:07:55 +0300

    sc

--- app/Http/Controllers/CommentParticipateController.php
+++ app/Http/Controllers/CommentParticipateController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\commentParticipate;
 use App\Http\Requests\StorecommentParticipateRequest;
 use App\Http\Requests\UpdatecommentParticipateRequest;
+use App\Models\participate;
+use App\Models\users;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -14,7 +16,23 @@ class CommentParticipateController extends Controller
     {
         $data = $request->all();
         $data['date_comment'] = Carbon::now('Asia/Riyadh')->format('Y-m-d H:i:s');
+        $data['user_id'] = auth('sanctum')->user()->id_user;
         $comment = commentParticipate::create($data);
+
+        $user = users::find(auth('sanctum')->user()->id_user);
+        $participate = participate::where('id_participate',$request->participate_id)->first();
+
+        $comment->nameUser = $user->nameUser;
+        $comment->img_image = $user->img_image;
+        $comment->user_id = $user->id_user;
+
+        $comment->name_participate = $participate->name_participate;
+        $comment->mobile_participate = $participate->mobile_participate;
+        $comment->namebank_participate = $participate->namebank_participate;
+        $comment->numberbank_participate = $participate->numberbank_participate;
+        $comment->add_date = $participate->add_date;
+        $comment->update_date = $participate->update_date;
+
         return $this->sendResponse($comment, 'Done');
     }
 
@@ -22,7 +40,7 @@ class CommentParticipateController extends Controller
     {
         $comments = CommentParticipate::where('participate_id', $id)
             ->orderBy('id', 'desc')
-            ->with('participates')
+            ->with('participates','users')
             ->get();
 
         return $this->sendResponse(
@@ -39,6 +57,9 @@ class CommentParticipateController extends Controller
                 'participate_id' => $comment->participate_id,
                 'content' => $comment->content,
                 'date_comment' => $comment->date_comment,
+                'user_id' => $comment->user_id,
+                'nameUser' => $comment->users->nameUser,
+                'img_image' => $comment->users->img_image,
                 'name_participate' => $comment->participates->name_participate,
                 'mobile_participate' => $comment->participates->mobile_participate,
                 'namebank_participate' => $comment->participates->namebank_participate,
