51b78e8d39cebec5f58b3e9535e628b723f7d3fb
anas <o@gamil.com>
2024-02-15 10:24:17 +0300

    LIKE

--- app/Imports/ClientsImport.php
+++ app/Imports/ClientsImport.php
@@ -28,15 +28,17 @@ class ClientsImport implements ToModel
             info('$row3 is: ' . $row[6]);
         } elseif ($row[6] == 'الموظف') {
             $name = 'ayaEng';
-        // } elseif ($row[6] == 'قمر'){
-        //     info('$row4 is: ' . $row[6]);
-        //     $name = 'قمر';
+        } elseif ($row[6] == 'قمر'){
+            info('$row4 is: ' . $row[6]);
+            $name = 'قمر';
         } else {
             $name = 'ayaEng';
         }
 
         info('name is: ' . $name);
-        $id_user = users::where('nameUser', $name)->first()->id_user;
+        // $id_user = users::where('nameUser', $name)->first()->id_user;
+        // Copy
+        $id_user = users::where('nameUser', 'LIKE', '%' . $name . '%')->first()->id_user;
 
 
         if ($row[9] == 'البريد الإلكتروني') {
