b5412b9c92e09880ec9684a24c55e5ac729cc7f8
anas <o@gamil.com>
2024-02-29 14:47:31 +0300

    counts of clients

--- app/Imports/AnotherDateClientsImport.php
+++ app/Imports/AnotherDateClientsImport.php
@@ -16,36 +16,32 @@ class AnotherDateClientsImport implements ToModel
 
     public function model(array $row)
     {
+        $counts = [];
+        $counts[0] = 0;
         $clientMobile = clients::where('mobile', $row[1])->first();
         if (!$clientMobile) {
             $clientName = clients::where('name_client', $row[2])->first();
             if (!$clientName) {
-                info('row1 is: ' . $row[0]);
+                info('line ');
                 if ($row[6] == 'عهود') {
                     $id_user = 208;
                     $name = 'عهود طرابزوني';
-                    info('$row1 is: ' . $row[6]);
                 } elseif ($row[6] == 'نوف') {
                     $name = 'نوف الجبرني';
                     $id_user = users::where('nameUser', 'LIKE', '%' . $name . '%')->first()->id_user;
-                    info('$row2 is: ' . $row[6]);
                 } elseif ($row[6] == 'سفيان') {
                     $id_user = 74;
                     $name = 'سفيان زيد';
-                    info('$row3 is: ' . $row[6]);
                 } elseif ($row[6] == 'الموظف') {
                     $id_user = 1;
                     $name = 'ayaEng';
                 } elseif ($row[6] == 'قمر') {
                     $id_user = 120;
-                    info('$row4 is: ' . $row[6]);
                     $name = 'قمر';
                 } else {
                     $id_user = 1;
                     $name = 'ayaEng';
                 }
-
-                info('name is: ' . $name);
 
 
                 if ($row[5] == 'إهتمام مختلف') {
@@ -53,20 +49,20 @@ class AnotherDateClientsImport implements ToModel
                 }
 
 
-                return new clients([
-                    'date_create' =>
-                    $row[0] == 'تاريخ التسجيل' || $row[0] === null ? null :
-                        Carbon::createFromDate(1899, 12, 30)->addDays($row[0])
-                        ->startOfDay()
-                        ->format('Y-m-d H:i:s'),
-                    'mobile' => $row[1],
-                    'name_client' => $row[2],
-                    'sourcclient' => $row[3],
-                    'type_record' => $row[4],
-                    'type_classification' => $row[5],
-                    'fk_user' => $id_user,
-                    'SerialNumber' => 1
-                ]);
+                // return new clients([
+                //     'date_create' =>
+                //     $row[0] == 'تاريخ التسجيل' || $row[0] === null ? null :
+                //         Carbon::createFromDate(1899, 12, 30)->addDays($row[0])
+                //         ->startOfDay()
+                //         ->format('Y-m-d H:i:s'),
+                //     'mobile' => $row[1],
+                //     'name_client' => $row[2],
+                //     'sourcclient' => $row[3],
+                //     'type_record' => $row[4],
+                //     'type_classification' => $row[5],
+                //     'fk_user' => $id_user,
+                //     'SerialNumber' => 1
+                // ]);
             }
         }
     }
