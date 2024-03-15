fe8ae6b105225300aa22031ca65d7604edbf8102
anas <o@gamil.com>
2024-03-16 00:29:26 +0300

    asd

--- app/Models/clients_date.php
+++ app/Models/clients_date.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Http\Request;
 
 class clients_date extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $table = 'clients_date';
     public $timestamps = false;
+    protected $primaryKey = 'idclients_date';
 
     protected $fillable = [
         'idclients_date',
@@ -29,4 +33,38 @@ class clients_date extends Model
         'fk_user_done',
         'date_update_visit'
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        info('dddd');
+        $request = app(Request::class);
+        $routePattern = $request->route()->uri();
+        $ip = $request->ip();
+        $user = auth('sanctum')->user();
+        info('fgh');
+        $userName = $user->nameUser;
+        return LogOptions::defaults()
+            ->logOnly(['*'])
+            ->logOnlyDirty()
+            ->useLogName('Client Date Log')
+            ->setDescriptionForEvent(function (string $eventName) use ($routePattern, $ip, $userName) {
+                // Provide the description for the event based on the event name, route pattern, and IP
+                if ($eventName === 'created') {
+                    return "Client Date created by $userName, using route: $routePattern from IP: $ip.";
+                } elseif ($eventName === 'updated') {
+                    return "Client Date updated by $userName, using route: $routePattern from IP: $ip.";
+                } elseif ($eventName === 'deleted') {
+                    return "Client Date deleted by $userName, using route: $routePattern from IP: $ip.";
+                }
+
+                // Default description if the event name is not recognized
+                return "Client Date action occurred by $userName, using route: $routePattern from IP: $ip.";
+            });
+    }
+
+    public function getQualifiedKeyName()
+    {
+        info('bbbb');
+        return $this->table . '.' . $this->primaryKey;
+    }
 }
--- app/Models/task.php
+++ app/Models/task.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Http\Request;
 
 class task extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = [
         'title',
@@ -40,6 +43,32 @@ class task extends Model
         'id_communication',
         'actual_delivery_date'
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        $request = app(Request::class);
+        $routePattern = $request->route()->uri();
+        $ip = $request->ip();
+        $user = auth('sanctum')->user();
+        $userName = $user->nameUser;
+        return LogOptions::defaults()
+            ->logOnly(['*'])
+            ->logOnlyDirty()
+            ->useLogName('Task Log')
+            ->setDescriptionForEvent(function (string $eventName) use ($routePattern, $ip, $userName) {
+                // Provide the description for the event based on the event name, route pattern, and IP
+                if ($eventName === 'created') {
+                    return "Task created by $userName, using route: $routePattern from IP: $ip.";
+                } elseif ($eventName === 'updated') {
+                    return "Task updated by $userName, using route: $routePattern from IP: $ip.";
+                } elseif ($eventName === 'deleted') {
+                    return "Task deleted by $userName, using route: $routePattern from IP: $ip.";
+                }
+
+                // Default description if the event name is not recognized
+                return "Task action occurred by $userName, using route: $routePattern from IP: $ip.";
+            });
+    }
 
     public function taskStatuses()
     {
