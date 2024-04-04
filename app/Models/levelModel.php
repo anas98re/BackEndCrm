da359d6b958d347e1e34d9335fc3e12c64e9de0d
Mustafa-Misho <sma.webdeve.backend.13@gmail.com>
2024-04-04 12:58:20 +0300

    remove previous log trait

--- app/Models/levelModel.php
+++ app/Models/levelModel.php
@@ -6,12 +6,11 @@ use App\Traits\Loggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Http\Request;
 
 class levelModel extends Model
 {
-    use HasFactory, LogsActivity, Loggable;
+    use HasFactory, Loggable;
 
     protected $table = 'level';
     public $timestamps = false;
@@ -22,31 +21,6 @@ class levelModel extends Model
         'periorty'
     ];
 
-    public function getActivitylogOptions(): LogOptions
-    {
-        $request = app(Request::class);
-        $routePattern = $request->route()->uri();
-        $ip = $request->ip();
-        $user = auth('sanctum')->user();
-        $userName = $user ? $user->nameUser : null;
-        return LogOptions::defaults()
-            ->logOnly(['*'])
-            ->logOnlyDirty()
-            ->useLogName('level Log')
-            ->setDescriptionForEvent(function (string $eventName) use ($routePattern, $ip, $userName) {
-                // Provide the description for the event based on the event name, route pattern, and IP
-                if ($eventName === 'created') {
-                    return "level created by $userName, using route: $routePattern from IP: $ip.";
-                } elseif ($eventName === 'updated') {
-                    return "level updated by $userName, using route: $routePattern from IP: $ip.";
-                } elseif ($eventName === 'deleted') {
-                    return "level deleted by $userName, using route: $routePattern from IP: $ip.";
-                }
-
-                // Default description if the event name is not recognized
-                return "level action occurred by $userName, using route: $routePattern from IP: $ip.";
-            });
-    }
 
     public function getQualifiedKeyName()
     {
--- app/Models/privg_level_user.php
+++ app/Models/privg_level_user.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use App\Traits\Loggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Http\Request;
 
 class privg_level_user extends Model
 {
-    use HasFactory, LogsActivity, Loggable;
+    use HasFactory, Loggable;
 
     protected $table = 'privg_level_user';
     protected $primaryKey = 'id_privg_user';
@@ -23,32 +21,6 @@ class privg_level_user extends Model
         'fk_privileg',
         'is_check'
     ];
-
-    public function getActivitylogOptions(): LogOptions
-    {
-        $request = app(Request::class);
-        $routePattern = $request->route()->uri();
-        $ip = $request->ip();
-        $user = auth('sanctum')->user();
-        $userName = $user ? $user->nameUser : null;
-        return LogOptions::defaults()
-            ->logOnly(['*'])
-            ->logOnlyDirty()
-            ->useLogName('privg_level_user Log')
-            ->setDescriptionForEvent(function (string $eventName) use ($routePattern, $ip, $userName) {
-                // Provide the description for the event based on the event name, route pattern, and IP
-                if ($eventName === 'created') {
-                    return "privg_level_user created by $userName, using route: $routePattern from IP: $ip.";
-                } elseif ($eventName === 'updated') {
-                    return "privg_level_user updated by $userName, using route: $routePattern from IP: $ip.";
-                } elseif ($eventName === 'deleted') {
-                    return "privg_level_user deleted by $userName, using route: $routePattern from IP: $ip.";
-                }
-
-                // Default description if the event name is not recognized
-                return "privg_level_user action occurred by $userName, using route: $routePattern from IP: $ip.";
-            });
-    }
 
     public function getQualifiedKeyName()
     {
