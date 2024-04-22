fe8eba894df51980689ad772460494316ff9043d
Anas Alefai <o@gamil.com>
2024-04-22 12:16:33 +0300

    fix updateCommunication date problem

--- app/Http/Resources/CommunicationResource.php
+++ app/Http/Resources/CommunicationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,7 +19,7 @@ class CommunicationResource extends JsonResource
             'id_communication' => (string)$this->id_communication,
             'fk_client' => (string)$this->fk_client,
             'fk_user' => (string)$this->fk_user,
-            'date_communication' => $this->date_communication,
+            'date_communication' => Carbon::parse($this->date_communication)->format('Y-m-d H:i:s'),
             'result' => $this->result,
             'notes' => $this->notes,
             'rate' => $this->rate,
