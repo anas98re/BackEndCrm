9f5c6c4fef16163ca7b372e56461cbffc2af2b11
Anas Alefai <o@gamil.com>
2024-04-24 09:27:17 +0300

    Updating UetCurrentUser name

--- routes/api.php
+++ routes/api.php
@@ -201,7 +201,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
 });
 Route::controller(UsersController::class)->group(function () {
-    Route::get('/returnCurrentUser', 'getCurrentUser');
+    Route::get('/UetCurrentUser', 'getCurrentUser');
 });
 
 Route::post('addEmailFromAdmin', [RegisterController::class, 'addEmailFromAdmin']);
