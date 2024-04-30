faeac552d9a8c93dc61f5f6ce572a32a757785d6
Mustafa-Misho <sma.webdeve.backend.13@gmail.com>
2024-04-30 09:21:11 +0300

    add id_invoice filter

--- app/Models/client_invoice.php
+++ app/Models/client_invoice.php
@@ -211,7 +211,7 @@ class client_invoice extends Model
                 ->orWhereHas('regoin', function ($query) use ($search) {
                     $query->where('name_regoin', 'like', '%' . $search . '%');
                 })
-                ->orWhere('id_invoice', $search)
+                ->orWhere('id_invoice', '=', $search)
             )
         );
 
